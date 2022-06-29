567372c6fc9d190bfa7838977356f77b23ebfe8d
taulant bytyqi <taulantbytyqi223@gmail.com>
2022-06-30 01:57:30 +0200

    created the funciton to render the emails

---
+++ src/main.ts
@@ -0,0 +1,97 @@
+import "./style.css";
+
+type Email = {
+  from: string;
+  header: string;
+  content: string;
+  emailAddress: string;
+  img: string;
+  read: boolean;
+  contains: boolean;
+};
+type Emails = {
+  emails: Email[];
+};
+
+const state: Emails = {
+  emails: [
+    {
+      from: "Nico",
+      header: "Link to today's video and slides is up!",
+      content:
+        "Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "nico@email.com",
+      img: "assets/nico.JPG",
+      read: false,
+      contains: true,
+    },
+    {
+      from: "Ed",
+      header:
+        "Congratulations! You have received a free beaver! Your name will now be displayed in the classroom's beaver list!",
+      content:
+        "Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "ed@email.com",
+      img: "assets/ed.JPG",
+      read: false,
+      contains: true,
+    },
+    {
+      from: "Government",
+      header: "Time to pay your tax!",
+      content:
+        "Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+      emailAddress: "government@email.com",
+      img: "assets/gov.jpg",
+      read: false,
+      contains: true,
+    },
+  ],
+};
+renderEmails();
+
+function renderEmails() {
+  let inboxTitle = document.createElement("h1");
+  inboxTitle.textContent = "Inbox";
+
+  document.querySelector("main").appendChild(inboxTitle);
+
+  let emailsUl = document.createElement("ul");
+  emailsUl.className = "emails-list";
+
+  let emails = getEmails(state.emails);
+  for (let email of emails) {
+    let emailList = document.createElement("li");
+    emailList.className = "emails-list__item";
+
+    let mark = document.createElement("span");
+    if (!email.read) {
+      mark.className = "emails-list__item__read-icon material-symbols-outlined";
+      mark.textContent = "mark_email_unread";
+    } else {
+      mark.className = "emails-list__item__read-icon material-symbols-outlined";
+      mark.textContent = "mark_email_read";
+    }
+
+    let userAvatar = document.createElement("img");
+    userAvatar.className = "emails-list__item__image";
+    userAvatar.src = email.img;
+
+    let senderName = document.createElement("p");
+    senderName.className = "emails-list__item__from";
+    senderName.textContent = email.from;
+
+    let emailContent = document.createElement("p");
+    emailContent.className = "emails-list__item__content";
+    emailContent.textContent = email.content;
+
+    emailList.append(mark, userAvatar, senderName, emailContent);
+    emailsUl.appendChild(emailList);
+  }
+  document.querySelector("main").appendChild(emailsUl);
+}
+
+function getEmails() {
+  let emails = state.emails.filter((email) => email.contains);
+  return emails;
+}
